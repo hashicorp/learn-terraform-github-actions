1f749a5a4aee71572b916f655281ad1a6d9a78b7
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-08-24 23:04:44 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# learn-terraform-github-actions
---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "remote" {
+    organization = "REPLACE_ME"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-830c94e3"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}
