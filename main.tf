c56a6cf6830c43cd598c30877d513ac91a294277
gold3nfish <gold3nfish@me.com>
2020-09-19 13:53:58 +0700

    change ami id

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "random" {}
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-08d70e59c07c61a3a"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
