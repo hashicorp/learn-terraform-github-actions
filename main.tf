88a785c54548f35f8520d11c12d0aa6c2fa3f556
Jose Manuel Holgueras Monedero <josemholgueras@gmail.com>
2020-09-30 09:59:23 +0200

    Hello Josema

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, Josema" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
