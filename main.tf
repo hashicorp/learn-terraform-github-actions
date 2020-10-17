b9025f95d5f983f619d37e41dde126e79f0b0648
abdelino17 <fare.abdel@gmail.com>
2020-10-17 13:40:30 +0200

    Point backend to correct TF org and workspace

--- main.tf
+++ main.tf
@@ -9,16 +9,16 @@ terraform {
   }
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "abtech-demo"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "gh-actions-tf"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 provider "random" {}
@@ -26,7 +26,7 @@ provider "random" {}
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0947d2ba12ee1ff75"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
