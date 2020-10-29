52304bf6816fc5eea0f5f543be989c7ba5cd1526
Martin Middleton <mmiddleton@actbluetech.com>
2020-10-29 13:14:22 -0400

    Adding the backend block

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 terraform {
+  backend "remote" {
+    organization = "MyABTestOrg"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
