ad93863592979ceda6d9859a3280a609d36b6eeb
gituser963 <73234527+gituser963@users.noreply.github.com>
2020-11-25 19:35:31 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source = "hashicorp/google"
     }
     random = {
       source = "hashicorp/random"
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Terraform-User-0054"
 
     workspaces {
       name = "gh-actions-demo"
@@ -17,36 +17,20 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-west-2"
+
+
+provider "google" {
+  version = "3.5.0"
+  #credentials = file(var.credential_file)
+  #project = var.project_id
 }
 
-provider "random" {}
 
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+#creating a storage bucket in GCP
+resource "google_storage_bucket" "bucket1" {
+  project = "vasu-pratice1-terraform-admin"
+  name = "API-test-bucket"
+  location = "us-east1"
+  storage_class = "REGIONAL"
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
