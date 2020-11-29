684988eb1b213a723a1b26330622e3079c3fe9f6
Groupware Technology <67933176+groupware-tech@users.noreply.github.com>
2020-11-28 19:01:40 -0800

    Point backend to HELLO TERR correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hellooooo, World" > index.html
+              echo "Hellooooo, TERRAFORM> index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
