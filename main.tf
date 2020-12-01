a7494e2075e004072f3a80fd0518b29122177afe
Rohan Arya <Rohan.Arya@infostretch.com>
2020-12-01 15:45:21 +0530

    ami change

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 provider "random" {}
@@ -26,7 +26,7 @@ provider "random" {}
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-08f63db601b82ff5f"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
