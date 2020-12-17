6f31dc016e095841d67bb5c7fb420d9973189415
Danny Smith <danny.smith@rackspace.co.uk>
2020-12-17 09:55:38 +0000

    Use Stockholm AMI ID

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "random" {}
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-57b13f29"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
