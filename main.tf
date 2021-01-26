69ed27e165a83cb7bcf07a2313846df56733a155
Sanjay <scshitole@gmail.com>
2021-01-25 20:58:01 -0800

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ terraform {
   }
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "SCStest"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "big-ci"
     }
   }
 }
