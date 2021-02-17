386ec2a5073e2d1e38fb5a88dc28c719a3912bfa
Jonathan Dias Baraldi <jonathanbaraldi@gmail.com>
2021-02-17 19:12:31 -0300

    mais ajustes

--- main.tf
+++ main.tf
@@ -46,15 +46,6 @@ resource "aws_instance" "web" {
               EOF
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
