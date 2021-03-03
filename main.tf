6031e5366e866ff97a2b0be90ccd2f43f5ab4acb
Kseniia Ryuma <kseniya0213@gmail.com>
2021-03-03 14:22:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,12 +33,19 @@ resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
+  
+  tags = {
+    Name = "github-actions-demo"
+    Owner = "Kseniia Ryuma"
+  }
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
+
+
 }
 
 resource "aws_security_group" "web-sg" {
