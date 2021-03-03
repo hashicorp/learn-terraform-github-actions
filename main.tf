bfa22ac9ab3ce38a1c6f8b771cf1810b2fb36141
Ugljesa Stosic <ugljesa.stosic@nutanix.com>
2021-03-03 01:58:06 +0100

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "TEAMAI"
 
     workspaces {
       name = "gh-actions-demo"
