109be1b0bb9a81967a0034d6d504084b6381a026
Kevin Ryan <kevinryan@Kevins-MacBook-Pro.local>
2021-03-17 11:07:45 +0100

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "pulp-training"
+    organization = "pulp-testing"
 
     workspaces {
       name = "gh-actions-demo"
