6ea2284be18c986a8109fcade86a4caaca68ade2
arimaverick <arimaverick@gmail.com>
2021-03-18 20:52:23 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Arimaverick"
 
     workspaces {
       name = "gh-actions-demo"
