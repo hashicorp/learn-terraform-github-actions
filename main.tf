b5623de3f328a9490cf0d48a868b89d329161b17
yuan <yuan.li@live.com>
2021-04-06 11:48:15 +0000

    point backend to coorect TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "beanxyz"
 
     workspaces {
       name = "gh-actions-demo"
