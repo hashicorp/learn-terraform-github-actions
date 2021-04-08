60921b6ffa1db0a4d67659bb9b78692c703eae70
zm-intern06 <zm-intern06@zmwarelab.com>
2021-04-08 20:13:17 +0200

    chaged second commit

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
+    azurem = {
+      source  = "hashicorp/azurem"
+      version = ">=2.0.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -21,36 +21,12 @@ terraform {
 }
 
 
-provider "aws" {
+provider "azurem" {
   region = "us-west-2"
 }
 
-
-
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+resource "azurerm_resource_group" "rg-hello-azure" {
+  name     = "rg-hello-azure"
+  location = "northcentralus"
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
