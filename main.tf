32912134326cce84b48fd4afb5976cefb224ccbd
Namit <nimiblz@hotmail.com>
2021-04-15 17:51:36 +1000

    fixed up incorrect main.tf file

--- main.tf
+++ main.tf
@@ -1,56 +1,32 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-  }
-  required_version = "~> 0.14"
-
-  backend "remote" {
-    organization = "REPLACE_ME"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }
+resource "azurerm_resource_group" "wus-rg" {
+  name     = "wus-vnet"
+  location = "West US"
 }
-
-
-provider "aws" {
-  region = "us-west-2"
-}
-
-
 
 resource "random_pet" "sg" {}
 
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
+# resource "aws_instance" "web" {
+#   ami                    = "ami-830c94e3"
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World" > index.html
+#               nohup busybox httpd -f -p 8080 &
+#               EOF
+# }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# resource "azurerm_" "web-sg" {
+#   name = "${random_pet.sg.id}-sg"
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
+# output "web-address" {
+#   value = "${aws_instance.web.public_dns}:8080"
+# }
