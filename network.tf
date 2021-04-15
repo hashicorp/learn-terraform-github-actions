180f5388ad4b67ece2b9b2d8f2a55d5b2266e57e
Namit <nimiblz@hotmail.com>
2021-04-15 21:43:05 +1000

    deleting resources before recreation

--- network.tf
+++ network.tf
@@ -1,8 +1,8 @@
-resource "azurerm_network_security_group" "wus-nsg" {
-  name                = "NSGS1"
-  location            = azurerm_resource_group.wus-rg.location
-  resource_group_name = azurerm_resource_group.wus-rg.name
-}
+# resource "azurerm_network_security_group" "wus-nsg" {
+#   name                = "NSGS1"
+#   location            = azurerm_resource_group.wus-rg.location
+#   resource_group_name = azurerm_resource_group.wus-rg.name
+# }
 
 # resource "azurerm_network_ddos_protection_plan" "ddos-plan" {
 #   name                = "ddospplan1"
@@ -10,35 +10,35 @@ resource "azurerm_network_security_group" "wus-nsg" {
 #   resource_group_name = azurerm_resource_group.wus-rg.name
 # }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "virtualNetwork1"
-  location            = azurerm_resource_group.wus-rg.location
-  resource_group_name = azurerm_resource_group.wus-rg.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+# resource "azurerm_virtual_network" "example" {
+#   name                = "virtualNetwork1"
+#   location            = azurerm_resource_group.wus-rg.location
+#   resource_group_name = azurerm_resource_group.wus-rg.name
+#   address_space       = ["10.0.0.0/16"]
+#   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  #   ddos_protection_plan {
-  #     id     = azurerm_network_ddos_protection_plan.ddos-plan.id
-  #     enable = false
-  #   }
+#     ddos_protection_plan {
+#       id     = azurerm_network_ddos_protection_plan.ddos-plan.id
+#       enable = false
+#     }
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
+#   subnet {
+#     name           = "subnet1"
+#     address_prefix = "10.0.1.0/24"
+#   }
 
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-  }
+#   subnet {
+#     name           = "subnet2"
+#     address_prefix = "10.0.2.0/24"
+#   }
 
-  subnet {
-    name           = "subnet3"
-    address_prefix = "10.0.3.0/24"
-    security_group = azurerm_network_security_group.wus-nsg.id
-  }
+#   subnet {
+#     name           = "subnet3"
+#     address_prefix = "10.0.3.0/24"
+#     security_group = azurerm_network_security_group.wus-nsg.id
+#   }
 
-  tags = {
-    environment = "Production"
-  }
-}
+#   tags = {
+#     environment = "Production"
+#   }
+# }
