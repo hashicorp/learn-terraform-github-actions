7b4df81a27253f5e3708be515dcca902fd9b5bbb
Namit <nimiblz@hotmail.com>
2021-04-15 22:44:03 +1000

    adding route tables and firewall subnet

--- network.tf
+++ network.tf
@@ -4,23 +4,12 @@ resource "azurerm_network_security_group" "wus-nsg" {
   resource_group_name = azurerm_resource_group.wus-rg.name
 }
 
-# resource "azurerm_network_ddos_protection_plan" "ddos-plan" {
-#   name                = "ddospplan1"
-#   location            = azurerm_resource_group.wus-rg.location
-#   resource_group_name = azurerm_resource_group.wus-rg.name
-# }
-
 resource "azurerm_virtual_network" "example" {
   name                = "virtualNetwork1"
   location            = azurerm_resource_group.wus-rg.location
   resource_group_name = azurerm_resource_group.wus-rg.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  #     ddos_protection_plan {
-  #       id     = azurerm_network_ddos_protection_plan.ddos-plan.id
-  #       enable = false
-  #     }
 
   subnet {
     name           = "subnet1"
@@ -38,7 +27,40 @@ resource "azurerm_virtual_network" "example" {
     security_group = azurerm_network_security_group.wus-nsg.id
   }
 
+  subnet {
+    name           = "AzureFirewallSubnet"
+    address_prefix = "10.0.4.0/24"
+  }
+
   tags = {
     environment = "Production"
   }
+}
+
+resource "azurerm_route_table" "wus-rt" {
+  name                = "example-routetable"
+  location            = azurerm_resource_group.wus-rg.location
+  resource_group_name = azurerm_resource_group.wus-rg.name
+
+  route {
+    name                   = "example"
+    address_prefix         = "0.0.0.0"
+    next_hop_type          = "VirtualAppliance"
+    next_hop_in_ip_address = "10.10.1.1"
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "wus-rt-association-1" {
+  subnet_id      = (azurerm_virtual_network.example.subnet[*])[0].id
+  route_table_id = azurerm_route_table.wus-rt.id
+}
+
+resource "azurerm_subnet_route_table_association" "wus-rt-association-2" {
+  subnet_id      = (azurerm_virtual_network.example.subnet[*])[1].id
+  route_table_id = azurerm_route_table.wus-rt.id
+}
+
+resource "azurerm_subnet_route_table_association" "wus-rt-association-3" {
+  subnet_id      = (azurerm_virtual_network.example.subnet[*])[2].id
+  route_table_id = azurerm_route_table.wus-rt.id
 }
