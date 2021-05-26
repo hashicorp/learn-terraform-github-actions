916131632609c7ab7ac2dabe31e24d34a1ea1438
Samaya Ramesh Radhakrishnan <samayaramesh.r@Samayas-MBP.bbrouter>
2021-05-26 17:08:04 +0530

    terraform github actions

--- main.tf
+++ main.tf
@@ -11,13 +11,13 @@ terraform {
   }
   required_version = "~> 0.14"
 
-/*  backend "remote" {
-    organization = "REPLACE_ME"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }*/
+#  backend "remote" {
+#    organization = "REPLACE_ME"
+#
+#    workspaces {
+#      name = "gh-actions-demo"
+#    }
+#  }
 }
 
 
