00cf8fbcbc0f08dbf65a77da10cdd0665bc34c6c
nonso inyiama <nonzo404@yahoo.com>
2021-06-12 15:08:27 +0100

    Point backend to correct TFC org and workspace1

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
       version = "3.0.1"
     }
   }
-  required_version = "~> 0.14"
+  required_version = ">= 0.12, < 0.13, < 1.0"
 
   backend "remote" {
     scello-works = "gh-actions-demo"
