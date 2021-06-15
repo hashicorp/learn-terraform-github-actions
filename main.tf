0cb963145b05e2bbf9a429aba018479e96721463
Chris Brien <Chris.Brien@CPowerEnergyManagement.com>
2021-06-15 10:20:03 -0400

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "0Downtime"
+    organization = "0Downtime1"
 
     workspaces {
       name = "gh-actions-demo"
