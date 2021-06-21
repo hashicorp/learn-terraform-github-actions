8cf0041fc1896f0cedfb0b061dc03c9a3a51f9b6
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2021-06-21 17:43:35 -0400

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Demos"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "demo-github-actions"
     }
   }
 }
