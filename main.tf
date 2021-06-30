0f0052f473452191145b1f2a8fb80b4a63d7d1a8
Milind Chaudhari <milind270989@gmail.com>
2021-06-30 14:44:20 -0400

    changed org name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "my_learnings"
 
     workspaces {
       name = "gh-actions-demo"
