71f77dd40550910114430f22435f45a11dae9e7c
bharath <bharathkumarcb159@gmail.com>
2021-08-04 17:08:24 +0530

    test

--- main.tf
+++ main.tf
@@ -12,17 +12,17 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "myorg"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "automatetfapply"
     }
   }
 }
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 
