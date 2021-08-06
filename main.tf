0d5b07ba9ad91ff024f2bfa4c038a485ddb16bd5
Noah Ucab <noah@iona.studio>
2021-08-06 08:35:54 +0800

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "example-org-e1f041"
 
     workspaces {
       name = "gh-actions-demo"
