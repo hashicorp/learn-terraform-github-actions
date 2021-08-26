b3e43872aab44ce502acded729d5fef34ddf2318
Vinayak <Vaneet.Vinayak@coxautoinc.com>
2021-08-26 18:54:17 -0400

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Cox-Automotive-Canada"
 
     workspaces {
       name = "gh-actions-demo"
