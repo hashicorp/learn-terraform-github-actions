f0b033424f96568f14e642678d8e0d5d05517b40
matsue <matsue@enrise-corp.co.jp>
2021-09-09 16:54:02 +0900

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "test-corporation"
 
     workspaces {
       name = "gh-actions-demo"
