b49cb1240ea1996a56bd962b365ab70ea9abfc9f
chrisurf <chrisurf@users.noreply.github.com>
2021-09-18 19:49:48 +0200

    Test github actions with terraform cloud

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "tf-organization"
 
     workspaces {
       name = "gh-actions-demo"
