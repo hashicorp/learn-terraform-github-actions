098c185287f1e181af2604deb232e543e2e2cd92
drybnicek <david.rybnicek@gmail.com>
2021-09-22 22:38:28 +0200

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "TechKeeper"
 
     workspaces {
       name = "gh-actions-demo"
