54fcb1c6964c0d862e69e042ca48335cb1bd68ef
root <root@terraform1.us-central1-a.c.prefab-shape-320918.internal>
2021-09-26 14:32:56 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+- organization = "REPLACE_ME"
++ organization = "OBELMEKKI"
 
     workspaces {
       name = "gh-actions-demo"
