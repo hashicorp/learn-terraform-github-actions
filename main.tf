20feddf7c60c9a43743fb1e6b9bba25338bf3532
root <root@LSITC-2021350.localdomain>
2021-09-28 15:18:43 +0900

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "demo_hjkim"
 
     workspaces {
       name = "gh-actions-demo"
