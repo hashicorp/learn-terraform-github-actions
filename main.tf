7dda5a397f2adde3adeafb70b77f08b7581b2cc4
Andrey Avtomonov <andreybavt@gmail.com>
2021-10-03 14:45:20 +0200

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "Dataiku"
+    organization = "Dataiku123"
 
     workspaces {
       name = "gh-actions-demo"
