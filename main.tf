3d6f479eb73f4957ed3b99f7e778df1a8ba2a26e
dipesh <dipeshgaws@gmail.com>
2021-10-21 06:58:43 +0530

    point backend to correct tfc org

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Techsoft"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
