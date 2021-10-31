377b33546006a807b8089199b38776da3432118e
Srini Personal <srinivasaraonalluri@Srinivasas-MacBook-Pro.local>
2021-10-30 22:15:47 -0400

    point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME_SRINI1"
+    organization = "REPLACE_ME_SRINI11"
 
     workspaces {
       name = "gh-actions-demo"
