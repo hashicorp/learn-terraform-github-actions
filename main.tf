b1a22a29273f7f73f21077954f6b3918d77f8faf
BuddhikaD <buddhikadananjaya55@gmail.com>
2021-10-31 23:19:28 +0530

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Satriaana"
 
     workspaces {
       name = "gh-actions-demo"
