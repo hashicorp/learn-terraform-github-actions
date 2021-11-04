07ce85ad41f2b2637788b6b29b1382c2fe38ec1c
anofort <oneplusvlad@gmail.com>
2021-11-04 12:01:10 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "gh-actions-terraform-demo"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "github-actions-demo"
     }
   }
 }
