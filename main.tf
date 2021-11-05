3a31ba91fbf2147ad6fdd976b99a974b74d18642
Conal <50038168@serc.ac.uk>
2021-11-05 11:45:23 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -1,56 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
+      source = "hashicorp/aws"
     }
     random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
+      source = "hashicorp/random"
     }
   }
-  required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+-   organization = "REPLACE_ME"
++   organization = "YOUR_ORGANIZATION_NAME"
 
     workspaces {
       name = "gh-actions-demo"
     }
   }
-}
-
-
-provider "aws" {
-  region = "us-west-2"
-}
-
-
-
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
 }
