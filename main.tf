ae4e12b6a67a4480420c78b06626caf6bba63d37
Tomoki Yamaguchi <toyamagu@cac.co.jp>
2021-11-19 22:46:29 +0900

    update backend

--- main.tf
+++ main.tf
@@ -9,20 +9,20 @@ terraform {
       version = "3.0.1"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 1.00"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "toyamagu"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "github-actions-demo"
     }
   }
 }
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-northeast-1"
 }
 
 
@@ -30,19 +30,24 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0e60b6d05dc38ff11"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
+  subnet_id = "${var.subnet_id}"
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
+  tags = { 
+    Name = "toyamagu-github-terraform"
+  }
 }
 
 resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+  name = "toyamagu-${random_pet.sg.id}-sg"
+  vpc_id = "${var.vpc_id}"
   ingress {
     from_port   = 8080
     to_port     = 8080
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+  description = "VPC Id"
+  type        = string
+  default     = "vpc-009956a4ce4683d3d"
+}
+
+variable "subnet_id" {
+  description = "Subnet Id"
+  type        = string
+  default     = "subnet-09b7a05a70348fb19"
+}
