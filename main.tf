d070ff571d6313b2db5c6ad4452bd906a47e0af9
Russell Johnston <rujohns2@cisco.com>
2021-11-23 16:10:42 -0700

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "rj-demo-tfc"
 
     workspaces {
       name = "gh-actions-demo"
