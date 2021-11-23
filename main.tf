3cf51d0d6817c6390955223e2ade88d05f0d7773
AnushaJoshy11 <anusha.joshell@gmail.com>
2021-11-23 06:07:55 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "TestAnushaOrg"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "gh-actions-demoAPI"
     }
   }
 }
