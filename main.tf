3d1d2b09381e80e58d756830a3f4a2ae713d7170
Michael DeNino <denino@protonmail.com>
2021-11-24 18:33:33 -0500

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "michael-acg-terraform-demos"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "demo-github-actions"
     }
   }
 }
