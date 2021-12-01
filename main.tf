ee44e27f28c3f79cf8de7d531b0cce45f5859233
Glenn Acheson <glenn@adjusterpro.com>
2021-12-01 16:13:25 -0600

    Test Github Actions

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, World!" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
