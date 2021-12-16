834ce4a65972b8c6ca330ba9655f7e4b6241d621
Joanne Lei <jolei@jolei-mac.coloradodomain>
2021-12-15 21:20:08 -0600

    change to vpc

--- main.tf
+++ main.tf
@@ -22,35 +22,57 @@ terraform {
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 
 
-resource "random_pet" "sg" {}
+#resource "random_pet" "sg" {}
 
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
+#resource "aws_instance" "web" {
+#  ami                    = "ami-830c94e3"
+#  instance_type          = "t2.micro"
+#  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+#  user_data = <<-EOF
+#              #!/bin/bash
+#              echo "Hello, World" > index.html
+#              nohup busybox httpd -f -p 8080 &
+#              EOF
+#}
+
+#resource "aws_security_group" "web-sg" {
+#  name = "${random_pet.sg.id}-sg"
+#  ingress {
+#    from_port   = 8080
+#    to_port     = 8080
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
+
+#output "web-address" {
+#  value = "${aws_instance.web.public_dns}:8080"
+#
+#}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  #tags = {
+  #  Terraform = "true"
+  #  Environment = "dev"
+  #}
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
+
