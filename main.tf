2121994bdd26e6ad1649fe452a27e239a31dea0b
jdnrdcs <86047343+jdnrdcs@users.noreply.github.com>
2021-12-25 18:56:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "jdnrdcs-organisation"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "test-tf-workspace"
     }
   }
 }
@@ -30,7 +30,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0b50645ac1261267b"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
