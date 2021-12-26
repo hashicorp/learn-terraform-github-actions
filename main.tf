039c7c9b01d5c458127af10fa7e17638c3cf8d36
redactxd <63176775+redactxd@users.noreply.github.com>
2021-12-26 17:25:35 -0600

    point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,17 +12,17 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "zerosumdefense"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "zerosumdefense-io"
     }
   }
 }
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 
