8935790caf3857fbf54e57c53de0cfe688f149fd
bhartu <Tulabanduvenkatalakshmi.bharathi@bhp.com>
2022-01-05 15:01:06 +0530

    added files

---
+++ meta.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "github-terraform-learnings"
+
+    workspaces {
+      name = "learn-terraform-github-actions"
+    }
+  }
+}
