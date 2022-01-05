e5b68acb171d26ef22074dbc288d36a1a7fa8321
bhartu <Tulabanduvenkatalakshmi.bharathi@bhp.com>
2022-01-05 14:55:51 +0530

    modified org details

--- main.tf
+++ main.tf
@@ -1,28 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-  }
-  required_version = ">= 1.1.0"
-
-  cloud {
-    organization = "REPLACE_ME"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }
-}
-
-
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 
@@ -30,7 +7,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0ed9277fb7eb570c9 "
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
@@ -51,6 +28,4 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
+
