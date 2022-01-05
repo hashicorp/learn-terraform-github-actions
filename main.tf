e0dd50b8dc422590d98693ef6cfd9b72662c3a9e
bhartu <Tulabanduvenkatalakshmi.bharathi@bhp.com>
2022-01-05 15:21:33 +0530

    fixed syntax

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0ed9277fb7eb570c9 "
+  ami                    = "ami-0ed9277fb7eb570c9"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
