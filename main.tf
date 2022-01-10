0a12e15313dfe07a145ff1c4aee10d86cd06c5a4
po3xmpgroup <pablo.ojeda@3xmgroup.com>
2022-01-10 14:47:24 -0300

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Demos-po3xmgroup"
 
     workspaces {
       name = "gh-actions-demo"
