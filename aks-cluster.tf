b0fda82c6b47654ad60fdf1d451892ad2d11b3bd
Marouan ABBESSI <marouan.abbessi@sap.com>
2022-01-22 00:43:54 +0100

    formated the aks-cluster.tf file

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -38,7 +38,7 @@ resource "azurerm_kubernetes_cluster" "default" {
 
   service_principal {
     client_id     = var.ARM_CLIENT_ID
-    client_secret = var.ARM_CLIENT_SECRET 
+    client_secret = var.ARM_CLIENT_SECRET
   }
 
   role_based_access_control {
