428085b3bf2f77ce0f4db7a30e0a3c665f090229
shreet123 <98025685+shreet123@users.noreply.github.com>
2022-02-09 17:51:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
-  instance_type          = "t2.nano"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
