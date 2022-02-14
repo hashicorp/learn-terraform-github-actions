532a8ffaf15de31bcfe9835773b63996555c6b7f
Chris Kendrick <kendrickcc@gmail.com>
2022-02-14 15:09:53 -0600

    update location and backend name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "CKP"
 
     workspaces {
       name = "gh-actions-demo"
@@ -20,12 +20,9 @@ terraform {
   }
 }
 
-
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
-
-
 
 resource "random_pet" "sg" {}
 
