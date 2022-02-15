a25f1dfbba44d531cfdfda8847ee712dc562417d
NetcraftAutomation <faizanrazasuser@gmail.com>
2022-02-15 02:31:37 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "GHA-tutorial"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "gha-tutorial"
     }
   }
 }
