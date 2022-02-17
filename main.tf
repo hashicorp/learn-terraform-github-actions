09bd72a520653473e7e282814aab1137753f1ff4
alex <alex@localhost.localdomain>
2022-02-17 04:08:54 -0800

    add default vpc teste3

--- main.tf
+++ main.tf
@@ -36,14 +36,14 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+  name   = "${random_pet.sg.id}-sg"
   vpc_id = module.vpc.vpc_id
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }  
+  }
 }
 
 output "web-address" {
