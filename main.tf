9391c732e70dd4c53ac4621bbc4ab3fc8482da12
Mwanda Mwanda <mwanda.mwanda@wonderlabz.com>
2022-02-20 11:31:00 +0200

    initial update

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "acg-terraform-greylord"
 
     workspaces {
       name = "gh-actions-demo"
