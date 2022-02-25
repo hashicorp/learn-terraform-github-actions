4c99bb5847a1df34872bcdf222db7a87af87ee79
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2022-02-24 21:29:23 -0800

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    #organization = "REPLACE_ME"
+    organization = "richyworld"
 
     workspaces {
       name = "gh-actions-demo"
