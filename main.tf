fbd3f8680f2b47743eaa0bc37f7d9064eb60c83f
lbourn-discovery <lionel_bourn@discovery.com>
2022-02-25 22:33:05 -0500

    change organization

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "gh-actions-demo"
+    organization = "example-org-2c6b5d"
 
     workspaces {
       name = "gh-actions-demo"
