48f823efde2388ed5b620ac9918abd6637287f13
Kieran Gates <kgates@ukcloud.com>
2022-03-04 13:47:25 +0000

    add ec2 name

--- main.tf
+++ main.tf
@@ -34,6 +34,10 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
+  tags = {
+    name = "test-instance"
+  }
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
