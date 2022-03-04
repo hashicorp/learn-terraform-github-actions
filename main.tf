a98a30e665e3a7dd0f3a4fa1ef8128fa816a1fdf
Kieran Gates <kgates@ukcloud.com>
2022-03-04 13:44:44 +0000

    update workflow and tfc org details

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "Kirimson-ACG-Terraform-Demos"
 
     workspaces {
       name = "gh-actions-demo"
