47a2b87fb3fb362384fa02a1dbd8d3dd5d310bbf
Jason King <jason.king@conversocial.com>
2022-03-05 18:00:46 +0000

    add org/workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Json-Terraform-Labs"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
