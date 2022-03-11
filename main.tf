ec5d69cdb0fca50ecfcefec32d55f714480cc960
Rachit Agarwal <98770232+rachitjellywall@users.noreply.github.com>
2022-03-11 12:31:50 -0800

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "YOUR_ORGANIZATION_NAME"
+    # organization = "branch_gg"
 
     workspaces {
       name = "gh-actions-demo"
