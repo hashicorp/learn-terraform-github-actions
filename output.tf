1dfe23e2df1175286f9e348088c796dae6e6c723
Vincent Gagnon <vincent.gagnon@gmail.com>
2022-05-07 12:07:31 -0400

    Point backend to correct TFC org and workspace

---
+++ output.tf
@@ -0,0 +1,3 @@
+output "resource_group_id" {
+  value = azurerm_resource_group.rg.id
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" {
+  default = "rg_Test_Terraform"
+}
+
+variable "vnet_name" {
+  default = "vnet_Test_Terraform"
+}
