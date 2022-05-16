e8e7a318d53d7d72234bbded2b9243c98f391303
David Sharpe <david.a.sharpe@cgi.com>
2022-05-16 12:28:44 -0700

    First commit.

--- main.tf
+++ main.tf
@@ -1,18 +1,14 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
+    keycloak = {
+      source = "mrparkers/keycloak"
+      version = ">= 3.0.0"
     }
   }
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "example-org-f085f7"
 
     workspaces {
       name = "gh-actions-demo"
@@ -20,60 +16,22 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-west-2"
+variable "secret" {
+  description = "The client secret"
+  type        = string
+  sensitive   = true
 }
 
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+provider "keycloak" {
+  client_id       = "terraform"
+  client_secret   = var.secret
+  url      = "https://common-logon-dev.hlth.gov.bc.ca"
+  realm           = "moh_applications"
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+resource "keycloak_openid_client" "clientName" {
+  realm_id            = "moh_applications"
+  client_id           = "new-client1"
+  access_type         = "CONFIDENTIAL"
+  name                = "hi mom"
 }
