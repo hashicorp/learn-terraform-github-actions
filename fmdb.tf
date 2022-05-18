9b33871fd519a136b6cc04946523e3bdce1d2b76
David Sharpe <david.a.sharpe@cgi.com>
2022-05-18 13:52:54 -0700

    Format.

--- fmdb.tf
+++ fmdb.tf
@@ -1,33 +1,33 @@
 resource "keycloak_openid_client" "FMDB" {
-  access_type                                = "CONFIDENTIAL"
+  access_type = "CONFIDENTIAL"
   # backchannel_logout_revoke_offline_sessions = false
-  backchannel_logout_session_required        = false
-  base_url                                   = "https://fmdbd.hlth.gov.bc.ca/FMDB"
-  client_authenticator_type                  = "client-secret"
-  client_id                                  = "FMDB"
-  consent_required                           = false
-  description                                = "Formulary Management Database"
-  direct_access_grants_enabled               = false
+  backchannel_logout_session_required = false
+  base_url                            = "https://fmdbd.hlth.gov.bc.ca/FMDB"
+  client_authenticator_type           = "client-secret"
+  client_id                           = "FMDB"
+  consent_required                    = false
+  description                         = "Formulary Management Database"
+  direct_access_grants_enabled        = false
   # display_on_consent_screen                  = false
-  enabled                                    = true
+  enabled = true
   # extra_config                               = {}
-  frontchannel_logout_enabled                = false
-  full_scope_allowed                         = false
-  implicit_flow_enabled                      = false
-  name                                       = "FMDB"
+  frontchannel_logout_enabled = false
+  full_scope_allowed          = false
+  implicit_flow_enabled       = false
+  name                        = "FMDB"
   # oauth2_device_authorization_grant_enabled  = false
-  realm_id                                   = "moh_applications"
-  service_accounts_enabled                   = false
-  standard_flow_enabled                      = true
-  use_refresh_tokens                         = false
-  valid_redirect_uris                        = [
+  realm_id                 = "moh_applications"
+  service_accounts_enabled = false
+  standard_flow_enabled    = true
+  use_refresh_tokens       = false
+  valid_redirect_uris = [
     "http://localhost:8080/*",
     "https://fmdbd.hlth.gov.bc.ca/*",
     "https://localhost:8081/*",
     "https://logontest7.gov.bc.ca/clp-cgi/logoff.cgi*",
   ]
-  web_origins                                = []
-  admin_url = ""
+  web_origins = []
+  admin_url   = ""
 }
 
 resource "keycloak_role" "MOHUSER" {
