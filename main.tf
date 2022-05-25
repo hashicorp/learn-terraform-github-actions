1e4262d560e1dda60dbbdd8a8e2832ad0592b3a5
raaha <rafael.haak@gmail.com>
2022-05-25 17:19:22 -0300

    Change instance name

--- main.tf
+++ main.tf
@@ -42,19 +42,11 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "my-new-server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
 }
 
 resource "aws_security_group" "web-sg" {
@@ -74,6 +66,4 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
 }
