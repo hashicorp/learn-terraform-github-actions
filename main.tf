0c921585bffad0ab7b95747f89c21398af9a8d0d
Keny Debo <kenydebo@gmail.com>
2022-05-25 16:48:50 -0400

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Omoba-Terraform-Demos"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
