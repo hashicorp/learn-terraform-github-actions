4df59187bbf4e3fbed887e9b1f7ec4e13f0dacc0
Satish Kumar Anandhan <iamsatish@live.com>
2022-05-29 23:02:05 +0100

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "TERRAFORM GIT ACTIONS"
 
     workspaces {
       name = "gh-actions-demo"
