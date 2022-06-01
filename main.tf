bbdf850ee877dbe21c1251393e97cbf5aac79210
Swann75 <christopher.michel2@gmail.com>
2022-06-01 15:37:29 +0200

    update aws credentials in main

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "chris_orga"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "learn-terraform-github-actions"
     }
   }
 }
