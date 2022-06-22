042e22404a47f144c89a2ea1384e5b4e9aea832a
Shri Sai Sadhana Natarajan <shrisais@users.noreply.github.com>
2022-06-22 16:31:10 -0700

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "hello-world-ci-cd"
 
     workspaces {
       name = "gh-actions-demo"
