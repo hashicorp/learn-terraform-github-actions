bce21355d1e01dd09f57a4a9b15fb60784c1f6e0
Guy Guetta <guetta@microfocus.com>
2022-06-26 10:00:30 +0300

    change Org name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "SimplePlan"
+    organization = "guetta14"
 
     workspaces {
       name = "TerraformCloudWithActions"
