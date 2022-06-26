f4bdfdd985c840b530b16d7c202fb504df4ff155
Guy Guetta <guetta@microfocus.com>
2022-06-26 10:36:34 +0300

    add name tag to instance

--- main.tf
+++ main.tf
@@ -47,6 +47,10 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
+  tags = {
+    Name = "Guy Test PR"
+  }
+
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
