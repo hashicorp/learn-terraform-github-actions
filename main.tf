b2aa59c210900549af0f2b305b0589e07de3b8a3
Armin <arminmesic@arminmesic-fvfg10p6q05p.fritz.box>
2022-06-27 12:20:37 +0200

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t2.small"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
