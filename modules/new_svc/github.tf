735fb88b78e25642c3bdd35aa03f3544cb74f46c
liaurora <you@example.com>
2022-06-28 02:53:49 +0000

    fix git provider

--- modules/new_svc/github.tf
+++ modules/new_svc/github.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
 resource "github_repository" "service_repo" {
   name        = "${var.service_name}-repo"
   description = "repo for ${var.service_name} microservice on gke"
