37464ef8e5f3a9d5a5285189ae787b3a11fe8b76
jsimonelliuhc <john.simonelli.uhc@gmail.com>
2022-06-30 22:09:25 -0500

    made the correct changes

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "Somethingnew"
+    organization = "random-Organeyezation"
 
     workspaces {
       name = "gh-actions-demo"
