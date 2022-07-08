5c3496dddb20d673c148db37dca651b3b950f0f8
zenghui <huihui127127@gmail.com>
2022-07-08 16:50:26 +0100

    added ec2 name

--- main.tf
+++ main.tf
@@ -47,6 +47,10 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
+  tags = {
+    Name = "GitHub Actions-Hello World Machine"
+  }
+
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
