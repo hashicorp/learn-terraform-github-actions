439f45251a466f0216ef4a5ccab78b3a4daff199
Asanka Nissanka <asanka@getshoutout.com>
2022-07-13 15:15:46 +0800

    updated org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "Moosarc"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "gh_actions_demo"
     }
   }
 }
