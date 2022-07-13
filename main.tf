bd9f274397286877f84092a9ccb03669e1d97a1b
Murali Pathivada <muralipathivada@gmail.com>
2022-07-13 11:22:31 -0400

    bucket access fixed

--- main.tf
+++ main.tf
@@ -175,6 +175,7 @@ data "aws_iam_policy_document" "example" {
       "s3:CreateBucket",
       "s3:DeleteBucket",
       "s3:GetBucketLocation",
+      "s3:PutBucketOwnershipControls"
     ]
 
     resources = [
