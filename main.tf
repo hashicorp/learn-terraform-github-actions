7fbe9f4b4f84cff88615764a55a84fc800812215
Murali Pathivada <muralipathivada@gmail.com>
2022-07-14 21:15:02 -0400

    one mgd pol

--- main.tf
+++ main.tf
@@ -193,7 +193,7 @@ resource "aws_ssoadmin_permission_set_inline_policy" "example" {
 
 # managed policies
 resource "aws_ssoadmin_managed_policy_attachment" "example" {
-  for_each           = toset(["arn:aws:iam::aws:policy/AWSLambda_FullAccess", "arn:aws:iam::aws:policy/AWSCloudTrailReadOnlyAccess"])
+  for_each           = toset(["arn:aws:iam::aws:policy/AWSLambda_FullAccess"])
   instance_arn       = tolist(data.aws_ssoadmin_instances.example.arns)[0]
   managed_policy_arn = each.value
   permission_set_arn = aws_ssoadmin_permission_set.example.arn
