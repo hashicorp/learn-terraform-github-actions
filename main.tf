da80e87537e477b828e3aadd1b69a877878b8472
Murali Pathivada <muralipathivada@gmail.com>
2022-07-14 21:32:07 -0400

    test1

--- main.tf
+++ main.tf
@@ -191,7 +191,7 @@ resource "aws_ssoadmin_permission_set_inline_policy" "example" {
   permission_set_arn = aws_ssoadmin_permission_set.example.arn
 }
 
-# managed policies
+# managed policies 1
 resource "aws_ssoadmin_managed_policy_attachment" "example" {
   for_each           = toset(["arn:aws:iam::aws:policy/AWSLambda_FullAccess"])
   instance_arn       = tolist(data.aws_ssoadmin_instances.example.arns)[0]
--- variables.tf
+++ variables.tf
@@ -3,3 +3,4 @@ variable "target_id" {
   description = "The Root ID, Organizational Unit ID, or AWS Account ID to apply SCPs."
   type        = string
 }
+
