2782791ec78b42dae55126924b458eed12242750
Dhakshna <81191286+Dhakshna2702@users.noreply.github.com>
2022-07-16 16:21:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "dhakshsolutions"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "Infra-Management"
     }
   }
 }
