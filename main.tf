deaf3ff53f8bc25467f2c6291600ad878af82dcf
Remco Niesten <r.niesten@uniserver.nl>
2022-07-19 09:59:24 +0200

    Main.tf ook aangepast met organisatie en eu-central-1

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "Uniserver"
 
     workspaces {
       name = "gh-actions-demo"
@@ -21,7 +21,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-central-1"
 }
 
 resource "random_pet" "sg" {}
