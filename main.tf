1a8c1ea97bf5fe3889ced498a4953e7ae1383e3d
Remco Niesten <r.niesten@uniserver.nl>
2022-07-19 11:56:31 +0200

    Meer tekst in de HTML file.

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
+              echo "<p>Hello World</p><p>Meer tekst hier.</p>" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
