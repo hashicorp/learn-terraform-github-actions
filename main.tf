daa6321afdbd6e6fe80cb8611c4e1fa169694641
samuellee-zy <samuelee.zy@gmail.com>
2022-07-20 21:14:12 +1000

    Point backend to appropriate TFC Workspace and org

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello Sam!" > /var/www/html/index.html
+              echo "Hello Sam!!" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
