e25bd5af89c8261902ab2dc265d5a460ee4305f5
samuellee-zy <samuelee.zy@gmail.com>
2022-07-20 21:43:14 +1000

    Updated name tag for ec2 instance

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   tags = {
-    name = "sam-gh-actions-demo"
+    Name = "sam-gh-actions-demo"
   }
 
   user_data = <<-EOF
