7ca27bb1e06f516b571554cc767c37952769d882
samuellee-zy <samuelee.zy@gmail.com>
2022-07-21 10:34:23 +1000

    Demo 1 - Name Tag updated for EC2 instance

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   tags = {
-    Name = "sam-gh-actions-demo"
+    Name = "MMC-gh-actions-demo-1"
   }
 
   user_data = <<-EOF
