03a1d1882ce521f3354f573e613995ffeb74df56
Manoel Victor Caxias Fernandes <kaxias4@gmail.com>
2022-07-20 22:19:07 -0300

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,16 +12,16 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "kpsti"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "git-actions-demo"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
