f754b38cea1e60b1fbccf6252303722eb0bd56a1
smok <smok@localhost.localdomain>
2022-08-02 14:43:27 +0200

    commit

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "smokgln	"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "test_terraform_aws_github"
     }
   }
 }
