1cf61c1cc682ee42544c3b33f4a1b914eb9fbe65
Dan-Santarossa <dpsantarossa@gmail.com>
2022-08-24 17:14:24 -0400

    final test after login

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "hashicorp-learn-cicd" # changed my org
+    organization = "hashicorp-learn-cicd" # test after login 
 
     workspaces {
       name = "gh-actions-demo"
