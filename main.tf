beff94bda5684d2b7d7ae7a7a28452a348cecb0e
darcyjwood <darcyjwood@gmail.com>
2022-08-26 20:26:12 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "gh-actions-demo"
+    organization = "darcyjwoodorg"
 
     workspaces {
       name = "gh-actions-demo"
