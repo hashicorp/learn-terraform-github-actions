f3c940ec738b818837f93ef45e61101efb8abf29
shake76 <carbafer@gmail.com>
2022-08-29 23:05:47 -0300

    fixing vpc

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,8 @@ module "vpc" {
   name = "frc-poc-vpc"
 
   cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  #azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs  = ["${var.region}a", "${var.region}b", "${var.region}c"] 
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
