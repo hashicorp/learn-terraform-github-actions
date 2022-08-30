10667cefd262b8ec353b9d256984efc5572ecd85
shake76 <carbafer@gmail.com>
2022-08-29 23:07:54 -0300

    tf fmt

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ module "vpc" {
 
   cidr = "10.0.0.0/16"
   #azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-  azs  = ["${var.region}a", "${var.region}b", "${var.region}c"] 
+  azs = ["${var.region}a", "${var.region}b", "${var.region}c"]
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
