43a8080f4ceb978c7d29a86eec5732b7c8606f14
shake76 <carbafer@gmail.com>
2022-09-01 17:17:51 -0300

    multienv test

---
+++ dev/deployment.tf
@@ -0,0 +1,46 @@
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example-dev"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+    namespace = random_string.namespace_name.result
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ prod/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+
+  required_version = "~> 1.0.11"
+
+  backend "remote" {
+    organization = "frc"
+    workspaces {
+      name = "gh-actions-prod"
+    }
+  }
+}
+
