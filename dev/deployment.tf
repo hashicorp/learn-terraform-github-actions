06a081b9a501b7aa005d48248d049bf0589d991e
shake76 <carbafer@gmail.com>
2022-09-02 08:37:24 -0300

    fixing deployment on devenv

--- dev/deployment.tf
+++ dev/deployment.tf
@@ -22,7 +22,7 @@ resource "kubernetes_deployment" "nginx" {
       }
       spec {
         container {
-          image = "nginx:1.7.5"
+          image = "nginx:1.7.4"
           name  = "example"
 
           port {
