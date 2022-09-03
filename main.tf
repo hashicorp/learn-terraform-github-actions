31d9f723171d35b1c8b898136429b70f77456a31
Ryan <ryan@hugo.health>
2022-09-03 11:35:28 -0400

    update terraform stuff

--- main.tf
+++ main.tf
@@ -4,15 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.26.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
   }
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "phytertek"
 
     workspaces {
       name = "gh-actions-demo"
@@ -23,57 +19,39 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+# Cluster
 
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+variable "app" {
+  default = "actions-tutorial"
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+resource "aws_kms_key" "kms-key" {
+    deletion_window_in_days = 7
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_cloudwatch_log_group" "log-group" {
+    name = "${var.app}-log-group"
+}
+resource "aws_ecs_cluster" "ecs-cluster" {
+    name = "${var.app}-ecs-cluster"
+    setting {
+        name = "containerInsights"
+        value = "enabled"
+    }
+
+    configuration {
+      execute_command_configuration {
+        kms_key_id = aws_kms_key.kms-key.arn
+        logging = "OVERRIDE"
+        log_configuration {
+            cloud_watch_encryption_enabled = true
+            cloud_watch_log_group_name = aws_cloudwatch_log_group.log-group.name
+        }
+      }
+    }
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+resource "aws_ecs_cluster_capacity_providers" "ecs-capacity-providers" {
+    cluster_name = aws_ecs_cluster.ecs-cluster.name
+    capacity_providers = ["FARGATE_SPOT", "FARGATE"]
 }
