7c9e79682f12c25a0e6b6cb1e6407e28bbd7d076
swinnytodd <swinnytodd225@gmail.com>
2022-09-06 12:28:15 +0300

    repeating test

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "swinnytodd_test2"
+    organization = "swinnytodd_test3"
 
     workspaces {
       name = "gh-actions-demo"
