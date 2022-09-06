189065b31e2164e6dd13e7e1d4b00028b99ed166
swinnytodd <swinnytodd225@gmail.com>
2022-09-06 12:06:18 +0300

    checking terraform fmt swinnytodd_test second commit

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "swinnytodd_test"
+    organization = "swinnytodd_test1"
 
     workspaces {
       name = "gh-actions-demo"
