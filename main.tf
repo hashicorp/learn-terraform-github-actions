5482eafe206d41b96f61914acdea12b37035c823
pwinning1991 <ftcpjw@yahoo.com>
2022-09-09 18:40:52 +0000

    Pont backend to correct tf org and workspace

--- main.tf
+++ main.tf
@@ -12,16 +12,16 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "pwinning1991-terrafrom-demos"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "demo-github-actions"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
