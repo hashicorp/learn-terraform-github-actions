5f3626c10485ce36ca1d13597dea97c8859b9c5f
lydia <105000328+lydiafadel@users.noreply.github.com>
2022-10-12 15:45:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web"{
   ami                    = "ami-02d0b1ffa5f16402d"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-0d77553dc9abeae0e"
@@ -32,4 +32,5 @@ resource "aws_instance" "web" {
 tags = {
     Name = "ExampleAppServer"
   }
+ }
 
