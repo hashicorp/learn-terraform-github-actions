cbdbb2b9214bb7950d8fe8619af53be87f1459c1
Imaze Enabulele <ienabulele84@gmail.com>
2022-10-31 16:23:08 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "mtc-terraform-12345"
 
     workspaces {
       name = "gh-actions-demo"
