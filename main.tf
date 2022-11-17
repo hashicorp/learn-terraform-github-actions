10be69a0a8bcf1b5a53fbb008cf8f9dd977b7f2e
Tony Welch <tony.welch@cloudreach.com>
2022-11-17 12:00:50 +0000

    TW: Changing instance type and message

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
@@ -52,7 +52,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
+              echo "Hello World, we are here for you!" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
