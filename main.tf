e391662709073e941ff8537faf49ade297a8d82a
ric <ericater193@gmail.com>
2022-11-23 04:54:42 +0000

    point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "Demo-oganisation"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "demo-workspace"
     }
   }
 }
