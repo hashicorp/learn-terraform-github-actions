d6fb71ee55d21dd68389b7b0b671ea6fc0a9e42d
ric <ericater193@gmail.com>
2022-11-23 07:01:05 +0000

    changed org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "nanaorg1"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "nanaworkspace1"
     }
   }
 }
