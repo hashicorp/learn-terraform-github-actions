032e87eb5ef24a94e70f3f18aef76f37728e43fc
Joey Solis <josephjsolis@gmail.com>
2022-11-27 10:00:04 -0800

    Point backend correct terraform config organization and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Demos-11272022"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "demo-github-actions"
     }
   }
 }
