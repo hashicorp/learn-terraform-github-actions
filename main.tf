853498fe3f37f1ee999a0207585afcad0bb9b082
warmmilkmedia <60011284+warmmilkmedia@users.noreply.github.com>
2022-11-29 18:49:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "warmmilkmedia"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "github-actions-pqt-automation-history-api"
     }
   }
 }
