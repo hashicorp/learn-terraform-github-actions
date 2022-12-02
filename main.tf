a5c6d38a6b5b4caf42338bce0442aa99afcc024e
yxa006 <yacin.bakir@gmail.com>
2022-12-02 13:20:41 -0500

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ terraform {
       version = "3.0.1"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.0"
 
-  cloud {
+  backend "remote" {
     organization = "ACG-Terraform-Demos"
 
     workspaces {
