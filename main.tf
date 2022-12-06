a1fdd962233704b7d9560bb04b806388f6c54277
yxa006 <yacin.bakir@gmail.com>
2022-12-06 15:31:09 -0500

    Point backend to the right VPC security group

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  vpc_security_group_ids = "sg-012825e9b1d4b2fba"
+  vpc_security_group_ids = ["${aws_security_group.web-sg.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
