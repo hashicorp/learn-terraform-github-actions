56f96f55ef56ac114a2326b8103e87e7627abbae
ravi <ravindrak041@gmail.com>
2022-12-10 21:48:41 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -12,16 +12,16 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "gh-actions-demo-ravi041"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "gh-actions-demo-2"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
