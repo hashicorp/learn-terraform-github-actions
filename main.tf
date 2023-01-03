468570fa671d4b3754180f8a83704b3000753824
SICPA-NET\IIsaj <ilir.isaj@sicpa.com>
2023-01-03 19:32:13 +0100

    changed main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "techsoal"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "demo-github-actions"
     }
   }
 }
@@ -52,7 +52,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
+              echo "Hello World - FIS Test" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
