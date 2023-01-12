2ec0ce3433cb6a507be61f61e6fe91b4d72a578c
mgabani-mri <117667088+mgabani-mri@users.noreply.github.com>
2023-01-12 12:33:28 -0500

    Point backend to correct tfc org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Demos"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
