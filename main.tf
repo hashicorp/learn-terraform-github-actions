22916f4f941ac6ad7e334cbb1edae5aef4653a1c
pkumar-c <pkumar-c@illuminateed.net>
2023-01-21 20:56:02 +0530

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "example-org-62014a"
 
     workspaces {
       name = "gh-actions-demo"
