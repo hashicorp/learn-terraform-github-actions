e403090aeb30fed9582bbb2d3f04213b3bb74546
phorind <paulo-henrique.orind@teliacompany.com>
2023-01-26 11:19:07 +0100

    no message

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
+    snowflake = {
+      source = "Snowflake-Labs/snowflake"
+      version = "0.55.1"
     }
   }
   required_version = ">= 1.1.0"
@@ -20,60 +16,32 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-west-2"
+# connect to your Snowflake account
+provider "snowflake" {
+    account = "vw14553"
+    region = "eu-west-1" # fill-in only if required
+    username = "teliaacademy"
+    password = "Telia2023" # do not use, we'll set an env var instead
+    role = "accountadmin"
 }
-
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+# create one "my_tf_database.my_tf_schema.my_tf_table" db table
+resource "snowflake_database" "database" {
+  name      = "my_tf_database"
 }
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+resource "snowflake_schema" "schema" {
+  database  = snowflake_database.database.name
+  name      = "my_tf_schema"
 }
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "snowflake_table" "table" {
+  database  = snowflake_database.database.name
+  schema    = snowflake_schema.schema.name
+  name      = "my_tf_table"
+column {
+    name     = "id"
+    type     = "int"
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  column {
+    name     = "data"
+    type     = "text"
   }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
 }
