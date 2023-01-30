96ef28e80605e7a2a71710223772d43f514068d8
Alan Szlosek <alan.szlosek@hashicorp.com>
2023-01-30 10:21:22 -0500

    add actions workflow permission, name for step, bump TF versions

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "4.52.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "3.4.3"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = "~> 1.3.0"
 
   cloud {
     organization = "REPLACE_ME"
