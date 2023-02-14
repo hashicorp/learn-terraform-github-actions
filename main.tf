8f8676395278bd084092e8bf11a249d3df0d962e
corneliusga228 <75066163+corneliusga228@users.noreply.github.com>
2023-02-14 09:35:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "costar_capstone1"
 
     workspaces {
       name = "gh-actions-demo"
