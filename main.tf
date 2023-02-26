8454fadf33a6d3d921c0f075e764e0d7622591b5
michael2697 <michael2697@hotmail.com>
2023-02-26 11:46:03 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -11,8 +11,9 @@ terraform {
   }
   required_version = ">= 1.1.0"
 
+## 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "QATIP2697"
 
     workspaces {
       name = "gh-actions-demo"
