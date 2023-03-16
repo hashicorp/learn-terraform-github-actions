86b1496f2ec72f941599064c7a2c2b86eee41feb
Hhamzaou <houssem.hamz.training@gmail.com>
2023-03-16 15:44:45 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "Hhamzaou-Terraform-Demos"
 
     workspaces {
       name = "gh-actions-demo"
