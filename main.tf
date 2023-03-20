b536d9fca7916233d3edbc7e82329f932e6b4937
Sunny Agrawal <agrawal143vinit@gmail.com>
2023-03-20 01:23:18 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "learning-for-good"
 
     workspaces {
       name = "gh-actions-demo"
