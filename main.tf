ef8a37a4e1eec9ac5411c55d2b324317647a5f39
Lukasz <lukasz.piskorz@gmail.com>
2023-05-10 07:55:19 +0200

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "ACG-Terraform-Demo102"
+    organization = "ACG-Terraform-Demos102"
 
     workspaces {
       name = "demo-github-actions"
