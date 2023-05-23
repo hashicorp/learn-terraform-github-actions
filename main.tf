b364ebe0549f5855477d4feb1d38a52c293871f1
EC2 Default User <ec2-user@ip-172-31-20-0.us-west-2.compute.internal>
2023-05-23 15:25:22 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "julgonza-organization"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "julgonza-workspace"
     }
   }
 }
