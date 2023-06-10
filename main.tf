196a0aeddfebb2d61b854cb8ee32f6cf3eeca228
LordGarithm <vicente.revuelto@gmail.com>
2023-06-10 21:14:22 +0200

    mierda

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Labs1974"
 
     workspaces {
-      name = "gh-actions-demo"
+      name ="demo-github-actions" 
     }
   }
 }
