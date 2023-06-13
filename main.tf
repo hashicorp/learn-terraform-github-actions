04a44ce72d81500141858cac540ec991390dffe9
hoshang82 <dildarm@hotmail.com>
2023-06-13 10:32:49 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -1,62 +1,43 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
+      version = "3.26.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "3.0.1"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = "~> 1.0"
 
-  cloud {
-    organization = "REPLACE_ME"
+  backend "remote" {
+    organization = "ORG-TF-MYLAB"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "ws-github-actions"
     }
   }
 }
 
+
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
+
+
 
 resource "random_pet" "sg" {}
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
               EOF
 }
 
@@ -66,13 +47,6 @@ resource "aws_security_group" "web-sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
