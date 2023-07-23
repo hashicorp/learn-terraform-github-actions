71f5f6f18eaddfbe936e27eb6c26bacd3b53f03f
Salah Alaoui <salah.alaoui@fastmail.com>
2023-07-23 13:41:49 +0100

    add organization and workspace names

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "salahalaoui"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "demo-github-action"
     }
   }
 }
